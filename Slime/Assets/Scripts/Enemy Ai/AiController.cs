a0bb5b897348c08a487dae53986b270236b7ced0
Loading212 <127059762+Loading212@users.noreply.github.com>
2023-03-21 14:25:29 -0700

    Ai detect script

---
+++ Slime/Assets/Scripts/Enemy Ai/AiController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float detectionRange = 5f;
+    public Transform target;
+    private Rigidbody2D rb2d;
+    private bool isFollowing = false;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 movement = Vector2.zero;
+        if (isFollowing && target != null)
+        {
+            movement = target.position - new Vector3(rb2d.position.x, rb2d.position.y, 0);
+        }
+
+        rb2d.velocity = movement.normalized * moveSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isFollowing = true;
+            target = other.transform;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isFollowing = false;
+            target = null;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+    }
+
+
+}
